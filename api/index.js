2c19a0c8aab4fabd826aed251b639e2e6d55290e
daniel mckee <danielmckee@daniels-mini.home>
2023-03-10 17:13:07 +0000

    Calling api

--- api/index.js
+++ api/index.js
@@ -20,9 +20,6 @@ app.get('/api/getList', (req,res) => {
             res.end(d)
         })
     }).catch(er => console.log(er));
-  const list = ["item1", "item2", "item3"];
-  res.json(list);
-  console.log('Sent list of items');
 });
 
 app.get('')
