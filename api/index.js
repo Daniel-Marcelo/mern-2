baeea9c0c1667c1a11a4ed8836475c26e7a8e405
daniel mckee <danielmckee@daniels-mini.home>
2023-03-10 17:09:41 +0000

    Calling api

--- api/index.js
+++ api/index.js
@@ -2,16 +2,28 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv')
 var cors = require('cors');
-const path = require('path')
+const path = require('path');
 
+// const got = require('got');
+const { pipeline } = require('stream');
 dotenv.config()
 app.use(cors());
 
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => {
+    return fetch(...args)
+});
+
+
 app.get('/api/getList', (req,res) => {
+    fetch('https://query2.finance.yahoo.com/v1/finance/search?q=AAPL').then(data => {
+        data.json().then(d => console.log(d))
+    }).catch(er => console.log(er));
   const list = ["item1", "item2", "item3"];
   res.json(list);
   console.log('Sent list of items');
 });
+
+app.get('')
 
 
 // Handles any requests that don't match the ones above
@@ -19,7 +31,7 @@ app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
 });
 
-// const port = process.env.API_PORT || 4000;
-// app.listen(port,  () => console.log(`LISTENING ON PORT ${port}`))
+const port = process.env.API_PORT || 4000;
+app.listen(port,  () => console.log(`LISTENING ON PORT ${port}`))
 
 module.exports = app;
